3b92ac03f08d9fdbce61c95a7a7993eb2e1809a4
andreafdev <andreafps.dev@gmail.com>
2023-12-14 13:05:02 -0300

    atualização projeto conta bancária

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,10 +1,12 @@
 package conta;
 
 public class Menu {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String [] args) {
+	
+		
+		
 	}
 
 }
+
+
